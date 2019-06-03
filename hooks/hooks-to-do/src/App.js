836f7d1ead1ae9c8247ba7c197d9b3a895037e9e
Alec <alec.jordan.cs@gmail.com>
2019-06-02 20:00:14 -0400

    add Upate functionality

--- hooks/hooks-to-do/src/App.js
+++ hooks/hooks-to-do/src/App.js
@@ -1,7 +1,21 @@
 import React, {useState} from 'react';
 import './App.css';
 
-const Todo = ({todo}) => <div className="todo">{todo.text}</div>;
+function Todo({todo, index, completeTodo}) {
+  return (
+    <div
+      className="todo"
+      style={{
+        textDecoration: todo.isCompleted ? 'line-through' : '',
+        color: todo.isCompleted ? 'red' : null,
+      }}>
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
+    </div>
+  );
+}
 
 function TodoForm({addTodo}) {
   const [value, setValue] = useState(['']);
@@ -27,9 +41,18 @@ function TodoForm({addTodo}) {
 
 function App() {
   const [todos, setTodos] = useState([
-    {text: 'Learn about react'},
-    {text: 'Explore context API'},
-    {text: 'Dive deeper into webpack'},
+    {
+      text: 'Learn about React',
+      isCompleted: false,
+    },
+    {
+      text: 'Meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'Build really cool todo app',
+      isCompleted: false,
+    },
   ]);
 
   const addTodo = text => {
@@ -37,11 +60,22 @@ function App() {
     setTodos(newTodos);
   };
 
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+          />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
